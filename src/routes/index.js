2e71440c18e5d92c52a8ee0868c0f53042448f56
bhuvan malik <bhuvanmalik007@gmail.com>
2018-05-13 18:58:49 +0530

    added verify route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,34 @@
 import express from 'express'
 var router = express.Router()
+import jwt from 'jsonwebtoken'
+const publicKey = process.env.NODE_ENV === 'development' ? require('../../secrets') : process.env.PUBLICKEY
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send({ title: 'Express' })
 })
+
+router.get('/verify', jwtcheckr)
+
+const jwtverifyPromise = token => new Promise((resolve, reject) => {
+  jwt.verify(token, publicKey, { algorithms: ['HS256'] }, function (err, decoded) {
+    if (err) {
+      reject(err)
+      return
+    }
+    resolve(decoded)
+  })
+})
+
+const jwtcheckr = async (req, res) => {
+  if (!req.headers['authorization']) {
+    res.send(res, 403, ({ 'boo': 'No Token' }))
+  }
+  try {
+    await jwtverifyPromise(req.headers['authorization'].split(' ')[1])
+  } catch (e) {
+    res.send(res, 403, ({ 'boo': 'Authentication Failed' }))
+  }
+}
 
 export default router
