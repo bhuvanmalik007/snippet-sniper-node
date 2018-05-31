4eca064710d138c6d816ecee3ff65fcb0f8599fc
bhuvan malik <bhuvanmalik007@gmail.com>
2018-06-01 00:01:42 +0530

    implemented cross-product for basic routes

--- src/routes/snippetRouter.js
+++ src/routes/snippetRouter.js
@@ -1,17 +1,20 @@
 import express from 'express'
-// import roleModel from '../models/roleModel'
+import { xprod } from 'ramda'
 
 const router = express.Router()
+
+const routeNames = ['/snippet']
+const httpVerbs = ['get', 'post']
 
 // const importer = async (modelName) => await import(`../models/${modelName}`)
 
 function Resolver(model) {
 
   return {
-    _findById: (req, res) => model.default.findById(req.id, function (err, item) {
+    get: (req, res) => model.default.findById(req.id, function (err, item) {
       res.send(item)
     }),
-    _save: (req, res) => {
+    post: (req, res) => {
       const item = new model.default(req.body.payload)
       item.save()
       res.send(item)
@@ -19,16 +22,11 @@ function Resolver(model) {
   }
 }
 
-['/snippet'].map(
-  x => router.get(x, Resolver(require('../models/' + x.slice(1) + 'Model.js'))._findById)
-)
+xprod(routeNames, httpVerbs).map(combo => router[combo[1]](combo[0],
+  Resolver(require('../models/' + combo[0].slice(1) + 'Model.js'))[combo[1]]))
 
-;['/snippet'].map(
-  x => router.post(x, Resolver(require('../models/' + x.slice(1) + 'Model.js'))._save)
-)
-
-// ;['snippet'].map(
-//   x => router.post(x, Resolver(roleModel(require('../models/' + x + 'Model.js'), x).fold())._save)
+// ['/snippet'].map(
+//   x => router.get(x, Resolver(require('../models/' + x.slice(1) + 'Model.js'))._findById)
 // )
 
 export default router
