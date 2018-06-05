cbdb8b737718252e5350480983f97b5b66f5f91c
bhuvan malik <bhuvanmalik007@gmail.com>
2018-06-06 03:07:50 +0530

    made MongoDb = Relational Db 😎

--- src/routes/baseRouter.js
+++ src/routes/baseRouter.js
@@ -5,22 +5,45 @@ const router = express.Router()
 
 const routeNames = ['/snippet/(:id)?', '/document/(:id)?']
 const httpVerbs = ['get', 'put', 'post', 'delete']
+const parentMapper = {
+  snippet: 'document'
+}
+
+const middleWareSpitter = (child, Resolver, method) => parentMapper[child] ?
+  [
+    Resolver(require('../models/' + child + 'Model.js').default)[method],
+    Resolver(require('../models/' + parentMapper[child] + 'Model.js').default).put
+  ]
+  :
+  Resolver(require('../models/' + child + 'Model.js').default)[method]
 
 function Resolver(model) {
   return {
     get: (req, res) => model.findById(req.params.id)
       .then(x => res.send(x)).catch(res.send),
-    post: (req, res) => {
+    post: (req, res, next) => {
       const item = new model(req.body)
       item.save()
-        .then(savedItem => res.send(savedItem))
+        .then(async savedItem => {
+          if (req.body.parentId) {
+            const child = req.path.split('/')[1]
+            const doc = await require('../models/' + parentMapper[child] + 'Model.js')
+              .default
+              .findById(req.body.parentId)
+              .exec()
+            req.params.id = req.body.parentId
+            req.body = {}
+            req.body[child + 's'] = [...doc[child + 's'], savedItem._id]
+            next()
+          }
+          else res.send(savedItem)
+        })
         .catch(res.send)
     },
     put: (req, res) => {
       model.findOneAndUpdate({ _id: req.params.id }, omit(['createdAt', 'updatedAt'], req.body), { new: true })
         .exec()
         .then(item => {
-          console.log(item)
           res.send(item)
         })
         .catch(res.send)
@@ -34,7 +57,9 @@ function Resolver(model) {
   }
 }
 
-xprod(routeNames, httpVerbs).map(combo => router[combo[1]](combo[0],
-  Resolver(require('../models/' + combo[0].split('/')[1] + 'Model.js').default)[combo[1]]))
+xprod(routeNames, httpVerbs).map(combo =>
+  router[combo[1]](combo[0],  // route path,
+    middleWareSpitter(combo[0].split('/')[1], Resolver, combo[1]) // middleware array
+  ))
 
 export default router
